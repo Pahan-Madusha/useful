f4836705aa3bb2f5d9aa81c9b96ce301b1c49072
Pahan <pahan.224@gmail.com>
2016-03-18 04:17:59 -0400

    Queue implementations

---
+++ Queue Data Structure using Linked List/queue.h
@@ -0,0 +1,162 @@
+#ifndef _QUEUE_
+#define _QUEUE_
+
+/*data structure for queue*/
+typedef struct 
+{
+	int capacity;
+	int size;
+	int front;
+	int rear;	
+	node_ref data;
+}que;
+
+typedef que* que_ref;
+
+/*Function prototypes*/
+
+/*
+  Allocate memory and create a new queue
+  No arguments
+  returns a pointer to a que
+*/
+que_ref queueCreate();
+
+/*
+  Free the memory allocated to queue
+  Arguments - pointer to a que
+  return 1 if successful
+  return 0 otherwise
+*/
+int queueDestroy(que_ref q);
+
+/*
+  Add an element to the queue
+  Arguments - pointer to que, integer
+  return 1 if successful
+  return 0 if queue if full
+*/
+int enqueue(que_ref q, int el);
+
+/*
+  Remove the least recently added element
+  Arguments - pointer to a que
+  Returns the removed element
+  Returns INT_MIN if queue is empty
+*/
+int dequeue(que_ref q);
+
+/*
+  Get the least recently added element
+  Arguments - pointer to a que
+  Returns the element 
+  Returns INT_MIN if queue is empty
+*/
+int queuePeek(que_ref q);
+
+/*
+  Check whether queue is empty
+  Arguments - pointer to a que
+  Returns 1 if queue is empty
+  Returns 0 otherwise*/
+int queueIsEmpty(que_ref q);
+
+/*
+  Check whether queue is full
+  Arguments - pointer to a que
+  Returns 1 if queue is full
+  Returns 0 otherwise
+*/
+int queueIsFull(que_ref q);
+
+/****************************/
+que_ref queueCreate()
+{
+	que_ref ref = malloc(sizeof(que));
+	ref -> capacity = SIZE;
+	ref -> size = 0;
+	ref -> front = 0;
+	ref -> rear = 0;
+
+	node_ref data = malloc(sizeof(node));
+	return ref;
+}
+
+/****************************/
+int queueDestroy(que_ref q)
+{
+	if(q == NULL)
+		return 0;
+	if(q -> data == NULL)
+		return 0;
+ 
+	free(q -> data);
+	free(q);
+
+	return 1;
+}
+
+/****************************/
+int enqueue(que_ref q, int el)
+{
+	if(queueIsFull(q))
+		return 0;
+	
+	add_to_tail(&(q -> data),el);
+	(q -> size)++;
+	(q -> rear)++;	
+	
+	if( (q -> rear) == SIZE )
+		(q -> rear) = 0;
+		
+	return 1;
+} 
+
+/****************************/
+int dequeue(que_ref q)
+{
+	if(queueIsEmpty(q))
+		return INT_MIN;
+
+	int val = peek_last_node(&(q -> data));
+
+	remove_last_node(&(q -> data));
+	(q -> front)++;
+
+	if( (q -> front) == SIZE)
+		q -> front = 0;
+	
+	(q -> size)--;
+
+	return val;
+}
+
+/****************************/
+int queuePeek(que_ref q)
+{
+	if(queueIsEmpty(q))
+		return INT_MIN;
+	
+	return peek_last_node(&(q -> data));
+}
+
+/****************************/
+int queueIsEmpty(que_ref q)
+{
+	if(q -> size == 0)
+		return 1;
+	return 0;
+	
+}
+
+/****************************/
+int queueIsFull(que_ref q)
+{
+	
+	if(q -> size == SIZE)
+		return 1;
+	return 0;
+}
+
+#endif
+
---
+++ Queue Data Structure using Linked List/test.c
@@ -0,0 +1,71 @@
+#define SIZE 9
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "linked_list.h"
+#include "queue.h"
+
+
+
+int main()
+{
+	int i;
+	que_ref q = queueCreate();//new queue
+
+	printf("Queue size - %d\n",SIZE);
+	printf("Pushing elements to queue\n\n");
+	
+	for(i = 0; i<10; i++)
+		if( enqueue(q,i+1) )
+			printf("Pushed %d successfully!\n",i+1);
+		else
+			printf("Pushing %d unsuccessful. Queue is full!\n",i+1);
+
+	printf("\n");
+	
+	for(i = 0; i<10; i++)
+		if( queuePeek(q) != INT_MIN )
+			printf("Dequeue - %d\n",dequeue(q));
+		else
+			printf("Dequeue unsuccessfull. Queue if empty!\n");
+
+	/************************************************************************/
+	printf("\n\n");
+	printf("Pushing elements to queue again\n\n");
+	
+	for(i = 0; i<10; i++)
+		if( enqueue(q,i+100) )
+			printf("Pushed %d successfully!\n",i+100);
+		else
+			printf("Pushing %d unsuccessful. Queue is full!\n",i+100);
+
+	printf("\n");
+	
+	printf("Dequeuing 5 elements\n");
+	for(i = 0; i<5; i++)
+		if( queuePeek(q) != INT_MIN )
+			printf("Dequeue - %d\n",dequeue(q));
+		else
+			printf("Dequeue unsuccessfull. Queue if empty!\n");
+
+	/************************************************************************/
+	printf("\n");
+	/*push again*/
+	printf("Pushing 6 elements again\n");
+	for(i = 0; i<6; i++)
+		if( enqueue(q,i+200) )
+			printf("Pushed %d successfully!\n",i+200);
+		else
+			printf("Pushing %d unsuccessful. Queue is full!\n",i+200);
+	
+	
+	printf("\n");
+	printf("Dequeueing all elements\n");
+	/*remove all*/
+	for(i = 0; i<9; i++)
+		if( queuePeek(q) != INT_MIN )
+			printf("Dequeue - %d\n",dequeue(q));
+		else
+			printf("Dequeue unsuccessfull. Queue if empty!\n");
+	return 0;
+}
